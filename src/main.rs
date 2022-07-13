b644f41af0ea5e89273cd897b4d9b70da31e7387
Veikko Soininen <veikko.soininen@gmail.com>
2022-07-13 17:37:07 +0300

    Add util which highlights selected tile

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,70 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
 use std::time::Duration;
 
 mod level;
 
+const TILE_SIZE: u32 = 20;
+const RENDER_MULTIPLIER: u32 = 2;
+const RENDER_SIZE: u32 = TILE_SIZE * RENDER_MULTIPLIER;
+
+fn get_tile_coordinates(id: u32) -> (u32, u32) {
+    let x = id * TILE_SIZE % 320;
+    let y = id * TILE_SIZE / 320 * TILE_SIZE;
+    (x, y)
+}
+
 fn get_block(id: u32) -> Rect {
-    const SIZE: u32 = 20;
-    let x = id * SIZE % 320;
-    let y = id * SIZE / 320 * SIZE;
-    Rect::new(x as i32, y as i32, SIZE, SIZE)
+    let (x, y) = get_tile_coordinates(id);
+    Rect::new(x as i32, y as i32, TILE_SIZE, TILE_SIZE)
+}
+
+fn highlight_selected_tile(id: u32, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+    canvas.set_draw_color(Color::from((255, 255, 255)));
+
+    let (x_logical, y_logical) = get_tile_coordinates(id);
+    let x = x_logical * RENDER_MULTIPLIER;
+    let y = y_logical * RENDER_MULTIPLIER;
+
+    draw_line(canvas, x, y, x, y + RENDER_SIZE - 1);
+    draw_line(canvas, x, y, x + RENDER_SIZE - 1, y);
+    draw_line(
+        canvas,
+        x + RENDER_SIZE - 1,
+        y,
+        x + RENDER_SIZE - 1,
+        y + RENDER_SIZE - 1,
+    );
+    draw_line(
+        canvas,
+        x,
+        y + RENDER_SIZE - 1,
+        x + RENDER_SIZE - 1,
+        y + RENDER_SIZE - 1,
+    );
+}
+
+fn draw_line(
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    x0: u32,
+    y0: u32,
+    x1: u32,
+    y1: u32,
+) {
+    let x0_signed = x0 as i32;
+    let y0_signed = y0 as i32;
+    let x1_signed = x1 as i32;
+    let y1_signed = y1 as i32;
+
+    canvas
+        .draw_line(
+            Point::from((x0_signed, y0_signed)),
+            Point::from((x1_signed, y1_signed)),
+        )
+        .unwrap();
 }
 
 pub fn main() {
@@ -64,6 +118,11 @@ pub fn main() {
             render_level(level, &mut canvas, &texture);
         }
 
+        highlight_selected_tile(0, &mut canvas);
+        highlight_selected_tile(1, &mut canvas);
+        highlight_selected_tile(16, &mut canvas);
+        highlight_selected_tile(17, &mut canvas);
+        highlight_selected_tile(15, &mut canvas);
         canvas.present();
 
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -79,10 +138,10 @@ fn render_level(
         for x in 0..level[0].len() {
             let src = get_block(level[y][x]);
             let dst = Rect::new(
-                (x * 40).try_into().unwrap(),
-                (y * 40).try_into().unwrap(),
-                40,
-                40,
+                (x * RENDER_SIZE as usize).try_into().unwrap(),
+                (y * RENDER_SIZE as usize).try_into().unwrap(),
+                RENDER_SIZE,
+                RENDER_SIZE,
             );
             canvas.copy(&texture, src, dst).unwrap();
         }
