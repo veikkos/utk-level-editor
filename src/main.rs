b1ba905948ed7313914008ae7f189bdbd28ad9e6
Veikko Soininen <veikko.soininen@gmail.com>
2022-07-16 23:47:40 +0300

    Inline few variables in context initialization

--- src/main.rs
+++ src/main.rs
@@ -15,32 +15,25 @@ use types::*;
 
 pub fn main() {
     let sdl = sdl2::init().unwrap();
-    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG);
+    let _image_context = sdl2::image::init(InitFlag::PNG);
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
     let video_subsystem = sdl.video().unwrap();
-
     let window = video_subsystem
         .window("Ultimate Tapan Kaikki - Level Editor", 640, 480)
         .position_centered()
         .build()
         .unwrap();
-
     let canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
-    let texture_floor = texture_creator.load_texture("./assets/FLOOR1.PNG").unwrap();
-    let texture_walls = texture_creator.load_texture("./assets/WALLS1.PNG").unwrap();
-
-    let font = ttf_context
-        .load_font("./assets/TheJewishBitmap.ttf", 128)
-        .unwrap();
-
     let mut context = Context {
         sdl,
         canvas,
         texture_creator: &texture_creator,
-        font,
-        texture_floor,
-        texture_walls,
+        font: ttf_context
+            .load_font("./assets/TheJewishBitmap.ttf", 128)
+            .unwrap(),
+        texture_floor: texture_creator.load_texture("./assets/FLOOR1.PNG").unwrap(),
+        texture_walls: texture_creator.load_texture("./assets/WALLS1.PNG").unwrap(),
         level: Level::get_default_level(),
         selected_tile_id: 0,
         texture_type_selected: TextureType::FLOOR,
